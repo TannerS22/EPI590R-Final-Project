07a6e4f3e9d2f94f086feddbd3be8119c6952e3c
Tanner Smalling <tanner.smalling@emory.edu>
2024-09-04 14:24:08 -0400

    Final commit

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,9 +1,32 @@
 library(tidyverse)
 library(gtsummary)
+library(here)
 
-# load and clean data
-cheeses_cols <- c(
-	"glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
-	"id", "nsibs", "samp", "race_eth", "sex", "region",
-	"income", "res_1980", "res_2002", "age_bir"
+
+pride <- read.csv(here::here("data", "pride_index.csv"))
+
+tbl_summary(
+	pride,
+	by = campus_name,
+	include = c(campus_name, students, rating),
+	label = list(
+		campus_name = "University",
+		students = "Student Population",
+		rating = "Rating"
+	))
+
+regression <-  lm(students ~ rating, data = pride)
+regression
+
+tbl_regression(
+	regression,
+	intercept = TRUE
 )
+
+hist(pride$students)
+totaluniversity <- function() {
+	amount <- nrow(pride)
+	return(amount)
+}
+totaluniversity()
+
---
+++ README.md
@@ -0,0 +1,2 @@
+Go to R directory and run Analysis.R.
+Next go to Docs and open and Render Document.qmd.
